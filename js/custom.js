eeca9860e2252da4b5a5df7db69b969b6cf10e9c
Steven Valles <s.valles94@hotmail.com>
2021-03-26 02:18:32 -0400

    no message

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+jQuery(function () {
+    new WOW({
+        mobile: false,
+    }).init();
+})
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
--- wp-content/themes/FLUID%20COFFEE%20THEME/js/custom.js
+++
@@ -1,5 +0,0 @@
-jQuery(function () {
-    new WOW({
-        mobile: false,
-    }).init();
-})
